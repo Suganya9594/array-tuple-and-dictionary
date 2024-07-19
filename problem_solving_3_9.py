a963553b13e1723bcda81c30e5ed62dda44bd11f
Suganya9594 <suganyaramlit@gmail.com>
2024-07-19 11:33:36 -0400

    Add files via upload

---
+++ problem_solving_3_9.py
@@ -0,0 +1,9 @@
+arr = [10, 20, 30, 9]
+target_sum = 59
+result = find_triplet_with_sum(arr, target_sum)
+
+if result:
+    print("Triplet found:", result)
+else:
+    print("No triplet found with the given sum.")
+    
